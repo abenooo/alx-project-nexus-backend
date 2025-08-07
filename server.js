87be41f149065685a3f8f4498d45272413491af6
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-08-07 12:57:27 -0700

    create aans add middleware db connection aand routes

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Connect to DB
+connectDB();
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/jobs', require('./routes/jobRoutes'));
+// app.use('/api/applications', require('./routes/applicationRoutes')); // upcoming
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
