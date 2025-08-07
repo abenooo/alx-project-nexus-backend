ecff7711339c908c0571e3e4915083ddd3389498
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-08-07 13:00:46 -0700

    finiish user controller

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+
+// @route GET /api/users/me
+const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
+
+// @route PUT /api/users/me
+const updateProfile = async (req, res) => {
+  try {
+    const updated = await User.findByIdAndUpdate(req.userId, req.body, { new: true }).select('-password');
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+};
+
+module.exports = { getProfile, updateProfile };
