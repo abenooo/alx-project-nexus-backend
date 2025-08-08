f90a165e67e36a120d03f4642ec9269dcaa1f72f
Abenezer Kifle <abenezerkifle000@gmail.com>
2025-08-08 00:56:47 -0700

    Update the model too include salady

--- models/Job.js
+++ models/Job.js
@@ -7,6 +7,7 @@ const jobSchema = new mongoose.Schema({
   country: String,
   region: String,
   jobType: String, // e.g., full-time, part-time
+  salary: Number,
   category: String,
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 }, { timestamps: true });
